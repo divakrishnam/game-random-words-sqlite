22bf31dfcd6869b6d38da8b0146ebb2a7b85c27e
Diva Krishna <divakrishnam@gmail.com>
2019-08-01 13:57:44 +0700

    update

---
+++ app/src/main/java/com/divakrishna/gamerandomwordssqlite/GameOverActivity.java
@@ -0,0 +1,49 @@
+package com.divakrishna.gamerandomwordssqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class GameOverActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tvYourScore;
+    private EditText etYourName;
+    private Button btnSave, btnHighScores, btnHome, btnPlayAgain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+        tvYourScore = findViewById(R.id.tv_your_score);
+        etYourName = findViewById(R.id.et_your_name);
+        btnSave = findViewById(R.id.btn_save);
+        btnSave.setOnClickListener(this);
+        btnHighScores = findViewById(R.id.btn_high_scores);
+        btnHighScores.setOnClickListener(this);
+        btnHome = findViewById(R.id.btn_home);
+        btnHome.setOnClickListener(this);
+        btnPlayAgain = findViewById(R.id.btn_play_again);
+        btnPlayAgain.setOnClickListener(this);
+
+        tvYourScore.setText(getIntent().getStringExtra("score"));
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_save:
+                break;
+            case R.id.btn_high_scores:
+                break;
+            case R.id.btn_home:
+                break;
+            case R.id.btn_play_again:
+                break;
+        }
+    }
+}
--- app/src/main/java/com/divakrishna/gamerandomwordssqlite/PlayActivity.java
+++ app/src/main/java/com/divakrishna/gamerandomwordssqlite/PlayActivity.java
@@ -1,13 +1,98 @@
 package com.divakrishna.gamerandomwordssqlite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PlayActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class PlayActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private DBSource dbSource;
+
+    private ArrayList<Words> values;
+
+    private TextView tvQuestion;
+    private EditText etAnswer;
+    private Button btnNext;
+
+    private int valuesLength;
+    private String answer;
+    private int count = 0;
+    private int score = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        tvQuestion = findViewById(R.id.tv_question);
+        etAnswer = findViewById(R.id.et_answer);
+        btnNext = findViewById(R.id.btn_next);
+        btnNext.setOnClickListener(this);
+
+        dbSource = new DBSource(this);
+        dbSource.open();
+
+        values = dbSource.getAllWord();
+
+        valuesLength = values.size();
+
+        NextWord(0);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_next:
+
+                    String answering = etAnswer.getText().toString().toLowerCase();
+                    if(answering.equals(answer)){
+                        Toast.makeText(this, "You are correct", Toast.LENGTH_SHORT).show();
+                        etAnswer.getText().clear();
+                        score += 10;
+                        count += 1;
+                        NextWord(count);
+                    }
+                    else{
+                        Toast.makeText(this, "You are wrong", Toast.LENGTH_SHORT).show();
+                        etAnswer.getText().clear();
+                        count += 1;
+                        NextWord(count);
+                    }
+
+                break;
+        }
+    }
+
+    private void NextWord(int i){
+        if(count < valuesLength){
+            String question = values.get(i).getQuestion().substring(0, 1).toUpperCase() + values.get(i).getQuestion().substring(1);
+            tvQuestion.setText(question);
+            answer = values.get(i).getAnswer().toLowerCase();
+        }
+        else{
+            Intent gameOver = new Intent(this, GameOverActivity.class);
+            gameOver.putExtra("score", String.valueOf(score));
+            startActivity(gameOver);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        dbSource.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        dbSource.close();
+        super.onPause();
     }
 }
